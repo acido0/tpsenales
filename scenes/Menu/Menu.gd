5d692e6c36ba21e04933365bcdaa809f94cd6db1
acido0 <valenvazquez606@gmail.com>
2022-05-01 18:13:22 -0300

    Instancia

--- scenes/Menu/Menu.gd
+++ scenes/Menu/Menu.gd
@@ -4,6 +4,22 @@ const INSTANCES_TEXT = "Cantidad de instancias: "
 # Variable para contar cantidad de instancias
 var instancesCount = 0
 
+const rojo = preload("res://scenes/Blocks/Red/BlockRed.tscn")
+const morado = preload("res://scenes/Blocks/Violet/BlockViolet.tscn")
+
+var cuborojo
+var cubomorado
+
+func generate_block():
+	cuborojo = rojo.instance()
+	cubomorado = morado.instance()
+	
+	pass
+
+func _process(delta):
+	generate_block()
+	pass
+
 func _ready():
 	# Inicializa el texto de "CountLabel" usando el texto
 	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
@@ -12,8 +28,10 @@ func _ready():
 
 
 func _on_RedGenerator_button_down():
+	get_parent().add_child(cuborojo)
 	pass # Replace with function body.
 
 
 func _on_VioletGenerator_button_down():
+	get_parent().add_child(cubomorado)
 	pass # Replace with function body.
